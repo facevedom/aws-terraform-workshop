e15d011a5c16fe35149564feb0753ef687e0f0b1
Felipe Acevedo Montoya <felipe.a.montoya@gmail.com>
2018-10-29 17:22:29 -0500

    Adds variables to state 2

--- state_2/ec2.tf
+++ state_2/ec2.tf
@@ -3,6 +3,41 @@ provider "aws" {
   profile = "${var.profile}"
 }
 
+data "template_file" "userdata" {
+  template = "${file("userdata.sh")}"
+}
+
+resource "aws_security_group" "aws_terraform_workshop" {
+  name        = "aws-terraform-workshop-sg"
+  description = "Allow HTTP and SSH access"
+  vpc_id      = "${var.vpc_id}"
+
+  ingress {
+    from_port   = "${var.app_port}"
+    to_port     = "${var.app_port}"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Project = "aws-terraform-workshop"
+  }
+}
+
 data "aws_ami" "latest_amazon_linux" {
   most_recent = true
 
@@ -14,21 +49,17 @@ data "aws_ami" "latest_amazon_linux" {
   owners = ["amazon"]
 }
 
-data "template_file" "userdata" {
-  template = "${file("zeppelin_userdata.sh")}"
-}
-
 resource "aws_instance" "psl_workshop" {
   ami                    = "${data.aws_ami.latest_amazon_linux.id}"
   instance_type          = "${var.instance_type}"
-  vpc_security_group_ids = "${var.vpc_security_group_ids}"
+  vpc_security_group_ids = ["${aws_security_group.aws_terraform_workshop.id}"]
   subnet_id              = "${var.subnet_id}"
   key_name               = "${var.key_name}"
   iam_instance_profile   = "${var.iam_instance_profile}"
   user_data              = "${data.template_file.userdata.rendered}"
 
   tags = {
-    "Name"    = "Terraform Workshop"
-    "Contact" = "avallecillac"
+    "Name"    = "hello-from-be"
+    "Project" = "aws-terraform-workshop"
   }
 }
--- state_2/userdata.sh
+++ state_2/userdata.sh
@@ -1,8 +1,1 @@
-#!/bin/bash
-cd /tmp/
-git clone https://github.com/facevedom/hello-from.git
-pip install -r requirements.txt
-FLASK_APP=hello_from.py flask run
-
-
-
+../state_1/userdata.sh
